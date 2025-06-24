b40511eb64b796a60ff299c8d9a7b0d160f9254a
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-06-24 11:04:41 +0700

    Jika nilai lebih kecil dari parent

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -55,8 +55,18 @@ class BinaryTree
 
             //5b = Exit
             return;
-
         }
+        
+         //step 6 = if the value in the data field of the new node is less than that of parent
+        if (x < parent -> info)
+        {
+            //6a = make the left child of parent point to the new node
+            parent->leftchild = newNode;
+
+            //6b =Exit
+            return;
+        }
+
 
 
     }
